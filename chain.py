165fdabe89e81cc61a821cab2a89603304be2fa3
DanBrink91 <DanBrink91@gmail.com>
2014-05-03 20:44:03 -0400

    updated to an actual markov model instead of always taking the same word

--- chain.py
+++ chain.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 words_after = {}
 pattern = re.compile('[^\w\s]+')
@@ -8,17 +9,18 @@ with open("MACBETH.txt") as source_file:
     for i in xrange(1, len(words)):
         word = words[i-1]
         if word in words_after:
-            words_after[word].add(words[i])
+            words_after[word].append(words[i])
         else:
-            words_after[word] = set(words[i])
+            words_after[word] = [words[i]]
 
 def generate_sentence(start, length):
     sentence = [start]
     count = 0
     word = start
     while word in words_after and count <= length:
-        word = words_after[word].pop()
+        word = random.choice(words_after[word])
         count += 1
         sentence.append(word)
     return ' '.join(sentence)
+    
 print generate_sentence('trifle', 10)
